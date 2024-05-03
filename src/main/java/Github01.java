4ecb776bf403df2bf16ad3438ae1f391039bb8b9
isa <ode121@gmail.com>
2024-05-03 09:22:27 +0200

    v1

---
+++ src/main/java/Github01.java
@@ -0,0 +1,10 @@
+public class Github01 {
+    public static void main(String[] args) {
+        // codes version 1
+        int x =5;
+        int y =7;
+        System.out.println(x-y);
+        System.out.println("done codes version 1");
+        String name="şinda";
+    }
+}
